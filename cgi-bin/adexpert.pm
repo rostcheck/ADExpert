b04131760cd8daee360c59c3c6e8adbdc6b46cd4
David Rostcheck <davidr@thisisdavidr.net>
2012-12-26 21:03:07 -0600

    Added A&D reports, ability to open and close books

--- cgi-bin/adexpert.pm
+++ cgi-bin/adexpert.pm
@@ -8,7 +8,45 @@ use ifaudit;
 $VERSION = 1.00;
 @ISA = qw(Exporter);
 
-@EXPORT = qw(&log_trading_partner_changes &log_firearm_changes &log_acquisition_changes &log_disposition_changes);
+@EXPORT = qw(&log_trading_partner_changes &log_firearm_changes &log_acquisition_changes &log_disposition_changes &prep_ad_book_fields);
+
+# Transform a row from teh ad_book_view (normalized database form) into the
+# un-normalized A&D book form per. ATF Quick References and Best Practices
+# Guide.
+sub prep_ad_book_fields
+{
+  my $r_state = $_[0];
+  my %return_hash;
+  my $importer = $r_state->{'importer'};
+  if (!$importer) { $importer = "No importer"; }
+  $return_hash{'manufacturer_or_importer'} = "$r_state->{'manufacturer'} / $importer";
+  if ($r_state->{'acquisition_ffl'}) {
+    $return_hash{'acquisition_info'} = "$r_state->{'acquisition_name'} FFL $r_state->{'acquisition_ffl'}";
+  }
+  else { 
+    $return_hash{'acquisition_info'} = "$r_state->{'acquisition_name'}<br>$r_state->{'acquisition_address1'}<br>$r_state->{'acquisition_city'}, $r_state->{'acquisition_state'} $r_state->{'acquisition_zip'}";
+  }
+  if ($r_state->{'disposition_ffl'}) {
+    $return_hash{'disposition_info'} = "FFL $r_state->{'disposition_ffl'}";
+  }
+  elsif ($r_state->{'4473_number'}) {
+    $return_hash{'disposition_info'} = "Form 4473 #$r_state->{'4473_number'}";
+  }
+  elsif ($r_state->{'lost_stolen_atf_incident_number'} || $r_state->{'lost_stolen_pd_incident_number'}) {
+    if ($r_state->{'lost_stolen_atf_incident_number'}) {
+      $return_hash{'disposition_info'} = "ATF Incident #$r_state->{'lost_stolen_atf_incident_number'}";
+    }
+    if ($r_state->{'lost_stolen_pd_incident_number'}) {
+      $return_hash{'disposition_info'} .= "<br>PD #$r_state->{'lost_stolen_pd_incident_number'}";
+    }
+  }
+  elsif($r_state->{'disposition_address1'}) {
+    $return_hash{'disposition_info'} = "$r_state->{'disposition_address1'}<br>$r_state->{'disposition_city'}, $r_state->{'disposition_state'} $r_state->{'disposition_zip'}";
+  }
+  else { $return_hash{'disposition_info'} = ""; }
+
+  return \%return_hash;
+}
 
 sub log_trading_partner_changes
 {
